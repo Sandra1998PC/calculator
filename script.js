6a8a9c1a3567b168ee69c8bde80b6af39164e8ef
Sandra1998PC <sandrapctla@gmail.com>
2021-12-03 16:44:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let displayvalue=(num)=>result.value+=num
+// function displayvalue(num){
+//     let text_box=document.querySelector("#result");
+//     text_box.value+=num
+
+// }
+let clearbox=()=>result.value=""
+// function clearbox(){
+//     document.querySelector("#result").value="";
+// }
+let evalueateexpression=()=>result.value=eval(result.value)
+// function evalueateexpression(){
+//     let text_box=document.querySelector("#result");
+//     text_box.value=eval(text_box.value)
+// }
+let Removeelement=()=>result.value=result.value.slice(0,-1)
+// function Removeelement(){
+//     let text_box=document.querySelector('#result');
+//     text_box.value=text_box.value.slice(0,-1)    
+// }
